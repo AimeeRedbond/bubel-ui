ab895e4b15ec385a37800c14671e5c61cee1f524
AimeeRedbond <aimeeredbond@gmail.com>
2019-11-23 19:10:36 +0000

    removes tutorial material

--- bubble_bank/lib/main.dart
+++ bubble_bank/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,68 +6,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'bubel',
+      title: 'bubbl',
       theme: ThemeData(          // Add the 3 lines from here...
         primaryColor: Colors.purpleAccent,
       ),
-      home: RandomWords(),
+      home: Banking(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
+class BankingState extends State<Banking> {
   final _settings = <String>['My account details', 'Set date and time', 'Swap current default'];
-  final _saved = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-        title: Text('bubel: banking made bubbly'),
+        title: Text('bubbl: banking made bubbly'),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.settings), onPressed: _pushSettings),
         ],
     ),
-    body: _buildSuggestions(),
-    );
-  }
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider(); /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      }
+    body: Center(
+      child: Text('Balance', style: _biggerFont,),
+    ),
     );
   }
 
@@ -104,7 +66,7 @@ class RandomWordsState extends State<RandomWords> {
   }
 }
 
-class RandomWords extends StatefulWidget {
+class Banking extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  BankingState createState() => BankingState();
 }
