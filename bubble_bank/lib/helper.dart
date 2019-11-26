48283e77175508d12495021cedcb5a2e16279fcb
fraserdscott <fraserdscott@gmail.com>
2019-11-26 21:46:41 +0000

    Move bubble view methods into its file

--- bubble_bank/lib/helper.dart
+++ bubble_bank/lib/helper.dart
@@ -13,21 +13,6 @@ List<Group> userGroups = <Group>[
   new Group("Other", '🤷‍♀️'),
 ];
 
-List<List> sortGroupRatios(Map<Group, double> groupRatios) {
-  List<List> sortedRatios = groupRatios.keys.map((Group group) {return [group, groupRatios[group]];}).toList();
-  sortedRatios.sort((List p1, List p2) => p1[1].compareTo(p2[1]));
-  return sortedRatios;
-}
-
-List<List> getRatios(Map<Group, List<Transaction>> groupTransactions, double total, groups) {
-  Map<Group, double> ratios = new Map.fromIterable(groups,
-      key: (item) => item,
-      value: (item) => 0);
-  for (Group group in groupTransactions.keys) {
-    ratios[group] = getBalance(groupTransactions[group])/total;
-  }
-  return sortGroupRatios(ratios);
-}
 
 void pushView(context, scaffold) {
   Navigator.of(context).push(
