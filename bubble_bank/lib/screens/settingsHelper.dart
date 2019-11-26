02212e940dfb4a7187627e20932fd7cc8502c61f
AimeeRedbond <aimeeredbond@gmail.com>
2019-11-26 21:24:50 +0000

    fix settingsHelper

--- bubble_bank/lib/screens/settingsHelper.dart
+++ bubble_bank/lib/screens/settingsHelper.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import '../helper.dart';
 
 Iterable<ListTile> settingsList(List<String> settings) {
   final Iterable<ListTile> tiles = settings.map(
