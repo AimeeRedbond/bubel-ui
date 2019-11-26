04df331e1f485f58ab3767dfb5dff26a43b2b905
fraserdscott <fraserdscott@gmail.com>
2019-11-26 16:54:17 +0000

    Create transaction template class

--- bubble_bank/lib/transaction.dart
+++ bubble_bank/lib/transaction.dart
@@ -1,6 +1,14 @@
 
+import 'package:flutter/material.dart';
+
 class Transaction {
   DateTime date;
   String description;
   double amount;
+
+  Transaction(DateTime date, String description, double amount) {
+    this.date = date;
+    this.description = description;
+    this.amount = amount;
+  }
 }
