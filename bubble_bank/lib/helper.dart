e5dcdb76deb9c3661a828ffaeea88ec589fc9a2f
AimeeRedbond <aimeeredbond@gmail.com>
2019-11-26 20:35:51 +0000

    remove unnecessary import

--- bubble_bank/lib/helper.dart
+++ bubble_bank/lib/helper.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:bubble_bank/transaction.dart';
 import 'package:bubble_bank/group.dart';
 
